1c87916ca521ebbd7c02c435c29e6f51c3846158
Andrew Nowak <a.nowak.2769@gmail.com>
2017-08-04 15:39:26 +0100

    Refactor installer
    Avoid using dict to list install instructions, use a list of objects.
    
    Could use a namedtuple, but it does not look straight forward to set
    defaults.

--- install-dots.py
+++ install-dots.py
@@ -9,71 +9,63 @@ dotdir = os.path.dirname(os.path.realpath(__file__))
 home = os.environ["HOME"]
 
 
-installs = {
-    dotdir + "/.vimrc": {
-        "to": home + "/.vimrc",
-        "postinstall": [
-            "curl -fLo {}/.vim/autoload/plug.vim --create-dirs "
-            "https://raw.githubusercontent.com/junegunn/vim-plug/master"
-            "/plug.vim".format(home),
-            "vim +PlugInstall +qall"
-        ]
-    },
-    dotdir + "/ftplugin": home + "/.vim/ftplugin",
-    dotdir + "/.gitconfig": home + "/.gitconfig",
-    dotdir + "/.bashrc": home + "/.bashrc",
-    dotdir + "/.bash_aliases": home + "/.bash_aliases",
-    dotdir + "/.abcde.conf": home + "/.abcde.conf"
-}
+class Dotfile:
+    def __init__(self, src, dest, postinstall=[], dir=False):
+        self.src = src
+        self.dest = dest
+        self.postinstall = postinstall
+        self.dir = dir
 
 
-def report_file_exists_err(conf_file, dest):
-    sys.stderr.write(("{} could not be installed as {} already exists "
-                     "on the file system.\n")
-                     .format(conf_file, dest))
+installs = [
+    Dotfile(dotdir + "/.vimrc",        home + "/.vimrc", postinstall=[
+        "curl -fLo {}/.vim/autoload/plug.vim --create-dirs "
+        "https://raw.githubusercontent.com/junegunn/vim-plug/master"
+        "/plug.vim".format(home),
+        "vim +PlugInstall +qall"
+    ]),
+    Dotfile(dotdir + "/ftplugin",      home + "/.vim/ftplugin", dir=True),
+    Dotfile(dotdir + "/.gitconfig",    home + "/.gitconfig"),
+    Dotfile(dotdir + "/.bashrc",       home + "/.bashrc"),
+    Dotfile(dotdir + "/.bash_aliases", home + "/.bash_aliases"),
+    Dotfile(dotdir + "/.abcde.conf",   home + "/.abcde.conf")
+]
+
+
+def report_file_exists_err(dotfile):
+    sys.stderr.write("{} could not be installed as {} already exists "
+                     "on the file system.\n"
+                     .format(dotfile.src, dotfile.dest))
 
 
 def main():
     code = 0
-    for src in installs:
-        dest = installs[src]
+    for dotfile in installs:
+        src = dotfile.src
+        dest = dotfile.dest
+
         try:
-            os.symlink(src, dest)
-        except TypeError:
-            # dest is not a string. Assume dict.
-            try:
-                os.symlink(src, dest["to"])
-            except FileExistsError:
-                # TODO prompt user whether to overwrite
-                if not (os.path.islink(dest["to"]) and os.readlink(dest["to"]) == src):
-                    # we have not already set up this symlink
-                    report_file_exists_err(src, dest["to"])
-                    code |= 1
-                continue
-
-            for cmd in dest["postinstall"]:
-                try:
-                    sp.run(cmd.split(' '), check=True)
-                except CalledProcessError as cpe:
-                    sys.stderr.write("Postinstall steps for {} failed: {}"
-                                     .format(src, cpe))
-                    code |= 2
-                    break
-
-            continue
-
+            os.symlink(src, dest, target_is_directory=dotfile.dir)
         except FileExistsError:
             # TODO prompt user whether to overwrite
             if not (os.path.islink(dest) and os.readlink(dest) == src):
                 # we have not already set up this symlink
-                report_file_exists_err(src, dest)
+                report_file_exists_err(dotfile)
                 code |= 1
             continue
+
+        for cmd in dotfile.postinstall:
+            try:
+                sp.run(cmd.split(' '), check=True)
+            except sp.CalledProcessError as cpe:
+                sys.stderr.write("Postinstall steps for {} failed: {}\n"
+                                 .format(src, cpe))
+                code |= 2
+                break
 
     return code
 
 
 if __name__ == "__main__":
     code = main()
-    if code > 0:
-        sys.exit(code)
+    sys.exit(code)
